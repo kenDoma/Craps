a5a2ff095f64d4adf18f81a03e9e013415f3fb20
Dominic Kang <dkang22@students.polytechnic.org>
2021-02-02 09:37:10 -0800

    Working die class and shell for craps

--- Craps.java
+++ Craps.java
@@ -8,5 +8,23 @@
 import java.util.Scanner;
 public class Craps
 {
+    public static void playGame()
+    {
+        
+    }
     
+    public static void main(String[] args)
+    {
+        Scanner in  = new Scanner(System.in);
+        System.out.println("--CRAPS--");
+        System.out.println("Would you like to play craps (y/n)? ");
+        String dec = in.nextLine();
+        while (dec.equals("y"))
+        {
+            playGame();
+            System.out.println("Would you like to play again (y/n)? ");
+            dec = in.nextLine();
+        }
+    }
+
 }
--- Die.java
+++ Die.java
@@ -7,5 +7,21 @@
  */
 public class Die
 {
-
+    // instance variables - replace the example below with your own
+    private int dieRoll;
+    
+    public Die()
+    {
+        dieRoll = 0;
+    }
+    
+    public void rollDie()
+    {
+        dieRoll = (int) (Math.random()*6+1);
+    }
+    
+    public int getRoll()
+    {
+        return dieRoll;
+    }
 }
